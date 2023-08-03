766f39582df832efdc52e43c4e6bb91062f4246e
Hironha <rafaelhirokato@gmail.com>
2023-08-02 22:00:20 -0300

    :recycle: rename state attr and change create_todo route method

--- todo-api-rs/src/presentation/rest_api/todo/mod.rs
+++ todo-api-rs/src/presentation/rest_api/todo/mod.rs
@@ -2,20 +2,20 @@ mod create;
 
 use crate::adapters::todo::store::TodoStore;
 
-use axum::{extract::FromRef, routing::get, Router};
+use axum::{extract::FromRef, routing::post, Router};
 use create::create_todo;
 
 #[derive(Clone, FromRef)]
 pub struct TodoState {
-    todo_creator: TodoStore,
+    todo_store: TodoStore,
 }
 
 pub fn create_router() -> Router {
     let state = TodoState {
-        todo_creator: TodoStore::new(),
+        todo_store: TodoStore::new(),
     };
 
     Router::new()
-        .route("/todos", get(create_todo))
+        .route("/todos", post(create_todo))
         .with_state(state)
 }
