dc9342fa5142cbf5e81e3173497bb337fe6adddf
Hironha <rafaelhirokato@gmail.com>
2023-09-05 20:02:07 -0300

    :recycle: remove associated functions

--- todo-api-rs/src/adapters/controllers/todo/update.rs
+++ todo-api-rs/src/adapters/controllers/todo/update.rs
@@ -2,7 +2,6 @@ use crate::adapters::dtos::todo::update::{Input, Output};
 use crate::application::functions::todo::{
     update_todo, Update, UpdateContext, UpdateError, UpdatePayload,
 };
-use crate::domain::todo::Todo;
 
 #[derive(Debug)]
 pub enum RunError {
@@ -22,43 +21,26 @@ impl<S: Update> UpdateController<S> {
 
 impl<S: Update> UpdateController<S> {
     pub async fn run(self, input: Input) -> Result<Output, RunError> {
-        let context = UpdateContext { store: self.store };
-        update_todo(&context, input.into_payload())
-            .await
-            .map(Output::from_todo)
-            .map_err(|e| e.run_error())
-    }
-}
+        let ctx = UpdateContext { store: self.store };
+        let payload = UpdatePayload {
+            id: input.id,
+            title: input.title,
+            description: input.description,
+            todo_at: input.todo_at,
+        };
 
-impl Input {
-    fn into_payload(self) -> UpdatePayload {
-        UpdatePayload {
-            id: self.id,
-            title: self.title,
-            description: self.description,
-            todo_at: self.todo_at,
-        }
-    }
-}
+        let todo = update_todo(&ctx, payload).await.map_err(|err| match err {
+            UpdateError::NotFound => RunError::NotFound,
+            UpdateError::InternalError => RunError::Internal,
+        })?;
 
-impl Output {
-    fn from_todo(todo: Todo) -> Self {
-        Self {
+        Ok(Output {
             id: todo.id.as_string(),
             title: todo.title,
             description: todo.description,
             todo_at: todo.todo_at.map(|at| at.ymd()),
             created_at: todo.created_at.rfc3339(),
             updated_at: todo.updated_at.rfc3339(),
-        }
-    }
-}
-
-impl UpdateError {
-    fn run_error(&self) -> RunError {
-        match self {
-            Self::NotFound => RunError::NotFound,
-            Self::InternalError => RunError::Internal,
-        }
+        })
     }
 }
