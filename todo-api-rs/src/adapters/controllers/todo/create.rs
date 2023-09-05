40c67f1c23082f121e194d7c0538651a2f8ff7e1
Hironha <rafaelhirokato@gmail.com>
2023-09-05 19:48:54 -0300

    :recycle: remove associated methods

--- todo-api-rs/src/adapters/controllers/todo/create.rs
+++ todo-api-rs/src/adapters/controllers/todo/create.rs
@@ -2,7 +2,6 @@ use crate::adapters::dtos::todo::create::{Input, Output};
 use crate::application::functions::todo::{
     create_todo, Create, CreateContext, CreateError, CreatePayload,
 };
-use crate::domain::todo::Todo;
 
 #[derive(Debug)]
 pub enum RunError {
@@ -22,40 +21,25 @@ impl<S: Create> CreateController<S> {
 impl<S: Create> CreateController<S> {
     pub async fn run(self, input: Input) -> Result<Output, RunError> {
         let context = CreateContext { store: self.store };
-        create_todo(&context, input.into_payload())
+        let payload = CreatePayload {
+            title: input.title,
+            description: input.description,
+            todo_at: input.todo_at,
+        };
+
+        let todo = create_todo(&context, payload)
             .await
-            .map(Output::from_todo)
-            .map_err(|e| e.run_error())
-    }
-}
+            .map_err(|err| match err {
+                CreateError::InternalError => RunError::Internal,
+            })?;
 
-impl Input {
-    fn into_payload(self) -> CreatePayload {
-        CreatePayload {
-            title: self.title,
-            description: self.description,
-            todo_at: self.todo_at,
-        }
-    }
-}
-
-impl Output {
-    fn from_todo(todo: Todo) -> Self {
-        Self {
+        Ok(Output {
             id: todo.id.as_string(),
             title: todo.title,
             description: todo.description,
             todo_at: todo.todo_at.map(|at| at.ymd()),
             created_at: todo.created_at.rfc3339(),
             updated_at: todo.updated_at.rfc3339(),
-        }
-    }
-}
-
-impl CreateError {
-    fn run_error(&self) -> RunError {
-        match self {
-            Self::InternalError => RunError::Internal,
-        }
+        })
     }
 }
