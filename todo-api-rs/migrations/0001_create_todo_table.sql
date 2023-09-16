ff82643de4c3fb2d68c09c46f397f65781d5e896
Hironha <rafaelhirokato@gmail.com>
2023-09-16 16:47:25 -0300

    :recycle: limit title and description length

--- todo-api-rs/migrations/0001_create_todo_table.sql
+++ todo-api-rs/migrations/0001_create_todo_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS Todo (
     id uuid UNIQUE NOT NULL,
-    title varchar NOT NULL,
-    description varchar,
+    title varchar(64) NOT NULL,
+    description varchar(256),
     todo_at date,
     created_at timestamptz NOT NULL,
     updated_at timestamptz NOT NULL,
