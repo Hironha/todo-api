95c293a3b515d61b3a9b5d7bb45151db04fe764a
Hironha <rafaelhirokato@gmail.com>
2023-09-29 21:21:36 -0300

    :art: rename methods to conventional names

--- todo-api-rs/src/domain/types/date.rs
+++ todo-api-rs/src/domain/types/date.rs
@@ -4,14 +4,14 @@ use time::{error, format_description::well_known::Rfc3339, macros::format_descri
 pub struct Date(time::Date);
 
 impl Date {
-    pub fn value(self) -> time::Date {
+    pub fn into_date(self) -> time::Date {
         self.0
     }
 
     /// Stringifies into YYYY-MM-DD
-    pub fn ymd(self) -> String {
+    pub fn to_ymd(self) -> String {
         let ydm_description = format_description!("[year]-[month]-[day]");
-        self.value().format(ydm_description).unwrap()
+        self.into_date().format(ydm_description).unwrap()
     }
 
     pub fn parse_str(input: &str) -> Result<Self, error::Parse> {
@@ -31,7 +31,7 @@ pub struct DateTime(time::OffsetDateTime);
 
 impl DateTime {
     /// Transforms into a string following RFC 3339 pattern
-    pub fn rfc3339(self) -> String {
+    pub fn to_rfc3339(self) -> String {
         self.0.format(&Rfc3339).unwrap()
     }
 }
