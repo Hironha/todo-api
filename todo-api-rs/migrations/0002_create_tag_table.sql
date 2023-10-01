aeecadb8549f78bc1c1a0a2da475fd981b53ab1c
Hironha <rafaelhirokato@gmail.com>
2023-09-30 22:28:26 -0300

    :bug: adjust sql on migrations

--- todo-api-rs/migrations/0002_create_tag_table.sql
+++ todo-api-rs/migrations/0002_create_tag_table.sql
@@ -1,8 +1,8 @@
-CREATE TABLE tag IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS tag (
     id uuid UNIQUE NOT NULL,
     name  varchar(64) NOT NULL,
     description varchar(128),
     created_at timestamptz NOT NULL,
     updated_at timestamptz NOT NULL,
     CONSTRAINT pk_tag PRIMARY KEY (id)
-)
+);
---
+++ todo-api-rs/migrations/0003_create_todo_tag_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS todo_tag (
+    todo_id uuid NOT NULL,
+    tag_id uuid NOT NULl,
+    CONSTRAINT pk_todo_tag PRIMARY KEY(todo_id, tag_id),
+    CONSTRAINT fk_todo_tag_todo_id FOREIGN KEY(todo_id) REFERENCES todo(id),
+    CONSTRAINT fk_todo_tag_tag_id FOREIGN KEY(tag_id) REFERENCES tag(id)
+);
