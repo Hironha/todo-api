f2cf27e726aea20383f07e04f9553fca11867c82
Hironha <rafaelhirokato@gmail.com>
2023-10-08 16:25:51 -0300

    :bug: make foreign keys delete cascade

--- todo-api-rs/migrations/0003_create_todo_tag_table.sql
+++ todo-api-rs/migrations/0003_create_todo_tag_table.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS todo_tag (
     todo_id uuid NOT NULL,
     tag_id uuid NOT NULl,
     CONSTRAINT pk_todo_tag PRIMARY KEY(todo_id, tag_id),
-    CONSTRAINT fk_todo_tag_todo_id FOREIGN KEY(todo_id) REFERENCES todo(id),
-    CONSTRAINT fk_todo_tag_tag_id FOREIGN KEY(tag_id) REFERENCES tag(id)
+    CONSTRAINT fk_todo_tag_todo_id FOREIGN KEY(todo_id) REFERENCES todo(id) ON DELETE CASCADE,
+    CONSTRAINT fk_todo_tag_tag_id FOREIGN KEY(tag_id) REFERENCES tag(id) ON DELETE CASCADE
 );
