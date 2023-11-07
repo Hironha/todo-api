03428b8a806b5e113b3e08c585baaca09f660421
Hironha <rafaelhirokato@gmail.com>
2023-11-06 21:29:13 -0300

    :sparkles: add created_at column

--- todo-api-rs/migrations/0003_create_todo_tag_table.sql
+++ todo-api-rs/migrations/0003_create_todo_tag_table.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS todo_tag (
     todo_id uuid NOT NULL,
     tag_id uuid NOT NULl,
+    created_at timestamptz NOT NULL,
     CONSTRAINT todo_tag_pk PRIMARY KEY(todo_id, tag_id),
     CONSTRAINT todo_tag_fk_todo_id FOREIGN KEY(todo_id) REFERENCES todo(id) ON DELETE CASCADE,
     CONSTRAINT todo_tag_fk_tag_id FOREIGN KEY(tag_id) REFERENCES tag(id) ON DELETE CASCADE
