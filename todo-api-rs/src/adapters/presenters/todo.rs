c426af2da0c7a3a35acf57da82770176fb370284
Hironha <rafaelhirokato@gmail.com>
2023-11-18 16:13:41 -0300

    :sparkles: add tags and done fields

--- todo-api-rs/src/adapters/presenters/todo.rs
+++ todo-api-rs/src/adapters/presenters/todo.rs
@@ -2,15 +2,19 @@ use serde::Serialize;
 
 use crate::domain::entities::todo::TodoEntity;
 
+use super::tag::TagPresenter;
+
 /// Presentable format of `TodoEntity`
 #[derive(Clone, Debug, Serialize)]
 pub struct TodoPresenter {
     pub id: String,
     pub title: String,
     pub description: Option<String>,
+    pub done: bool,
     /// `Date` in UTC YYYY-MM-DD format
     #[serde(rename(serialize = "todoAt"))]
     pub todo_at: Option<String>,
+    pub tags: Vec<TagPresenter>,
     /// `Date` in `RFC-3339` format
     #[serde(rename(serialize = "createdAt"))]
     pub created_at: String,
@@ -21,11 +25,19 @@ pub struct TodoPresenter {
 
 impl From<TodoEntity> for TodoPresenter {
     fn from(todo: TodoEntity) -> Self {
+        let tag_presenters = todo
+            .tags
+            .into_iter()
+            .map(TagPresenter::from)
+            .collect::<Vec<TagPresenter>>();
+
         Self {
             id: todo.id.to_string(),
             title: todo.title.into_string(),
             description: todo.description.map(|d| d.into_string()),
+            done: todo.done,
             todo_at: todo.todo_at.map(|at| at.to_ymd()),
+            tags: tag_presenters,
             created_at: todo.created_at.to_rfc3339(),
             updated_at: todo.updated_at.to_rfc3339(),
         }
