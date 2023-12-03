26700be3e23730b07777aef94a009f98e47f8e45
Hironha <rafaelhirokato@gmail.com>
2023-12-03 17:23:09 -0300

    :construction: make bare Result type

--- todo-app-solidjs/src/core/utils/result.ts
+++ todo-app-solidjs/src/core/utils/result.ts
@@ -1,11 +1,77 @@
-export type Ok<T = void> = T extends void
-  ? { status: "ok" }
-  : T extends undefined
-  ? { status: "ok"; data?: T }
-  : { status: "ok"; data: T };
+export type ResultUnion<T, E> = { kind: "ok"; value: T } | { kind: "err"; value: E };
 
-export type Err<E = void> = E extends void
-  ? { status: "err" }
-  : E extends undefined
-  ? { status: "err"; error?: E }
-  : { status: "err"; error: E };
+export class Result<T, E> {
+  private variant: Ok<T> | Err<E>;
+
+  private constructor(variant: Ok<T> | Err<E>) {
+    this.variant = variant;
+  }
+
+  static fromUnion<S, U>(union: ResultUnion<S, U>): Result<S, U> {
+    if (union.kind === "ok") {
+      return new Result<S, U>(new Ok(union.value));
+    } else {
+      return new Result<S, U>(new Err(union.value));
+    }
+  }
+
+  static ok<S, U>(value: S): Result<S, U> {
+    return new Result<S, U>(new Ok(value));
+  }
+
+  static err<S, U>(value: U): Result<S, U> {
+    return new Result<S, U>(new Err(value));
+  }
+
+  get value(): T | E {
+    return this.variant.value;
+  }
+
+  isOk(): this is Ok<T> {
+    return this.variant instanceof Ok;
+  }
+
+  isErr(): this is Err<E> {
+    return this.variant instanceof Err;
+  }
+
+  map<U>(predicate: (value: T) => U): Result<U, E> {
+    if (this.isOk()) {
+      return Result.ok(predicate(this.value));
+    } else {
+      return this as unknown as Result<U, E>;
+    }
+  }
+
+  union(): ResultUnion<T, E> {
+    if (this.isOk()) {
+      return { kind: "ok", value: this.value };
+    } else {
+      return { kind: "err", value: this.value as E };
+    }
+  }
+}
+
+export class Ok<T> {
+  private v: T;
+
+  constructor(value: T) {
+    this.v = value;
+  }
+
+  get value(): T {
+    return this.v;
+  }
+}
+
+export class Err<E> {
+  private v: E;
+
+  constructor(value: E) {
+    this.v = value;
+  }
+
+  get value(): E {
+    return this.v;
+  }
+}
