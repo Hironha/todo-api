2f2197ab399c5b78407aadbdcf427cc2295d903d
Hironha <rafaelhirokato@gmail.com>
2024-02-09 19:36:00 -0300

    :lipstick: rename from tag_repository to repository

--- todo-api-rs/src/application/use_cases/tag/delete.rs
+++ todo-api-rs/src/application/use_cases/tag/delete.rs
@@ -4,16 +4,16 @@ use crate::domain::types::Id;
 
 #[derive(Debug)]
 pub struct DeleteTagUseCase<T> {
-    tag_repository: T,
+    repository: T,
 }
 
 impl<T: TagRepository> DeleteTagUseCase<T> {
-    pub fn new(tag_repository: T) -> Self {
-        Self { tag_repository }
+    pub fn new(repository: T) -> Self {
+        Self { repository }
     }
 
     pub async fn exec(&self, tag_id: Id) -> Result<(), DeleteTagError> {
-        self.tag_repository
+        self.repository
             .delete(tag_id)
             .await
             .map_err(|err| match err {
