1b725fbcf0d8c832b75e0a8c75597cd80f13d793
Hironha <rafaelhirokato@gmail.com>
2024-02-11 14:45:20 -0300

    :fire: remove frontend prototype

--- todo-app-solidjs/src/core/utils/tag.ts
+++
@@ -1,6 +0,0 @@
-declare const __tag: unique symbol;
-
-export type Tagged<TName extends string> = { [__tag]: TName };
-
-/** Create a tagged type, i.e. opaque data type */
-export type Tag<T, TName extends string> = T & Tagged<TName>;
--- todo-app-solidjs/src/core/utils/unreachable.ts
+++
@@ -1,5 +0,0 @@
-/** Useful to safely assert a unreachable situation */
-export function unreachable(v: never): never {
-  console.error(`Reached unreachable case with value ${v}`)
-  return v
-}
